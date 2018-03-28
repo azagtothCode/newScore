fe6dce3a9ffdc2f4529c9556e3052c03a856820f
Administrador <umgp150992@gmail.com>
2018-03-27 21:39:51 -0600

    hola

--- js/apiCalls/legislators/callLegApi.js
+++ js/apiCalls/legislators/callLegApi.js
@@ -7,6 +7,20 @@ var supurl='http://localhost:8000';
 function openPerfilPage (url){
   window.open(url)
 }
+
+app.controller('classCtrl', function ($scope) {
+
+	$scope.isActive = false;
+
+  $scope.activeButton = function() {
+    $scope.isActive = !$scope.isActive;
+  }
+
+  $scope.activeButtonDouble = function() {
+    $scope.isActived = !$scope.isActived;
+  }
+});
+
 app.controller('deputyController', ['$scope', '$http', '$cacheFactory', function ($scope, $http, $cacheFactory) {
   var lruCache = $cacheFactory('lruCache', { capacity:0 });
 
@@ -19,15 +33,35 @@ app.controller('deputyController', ['$scope', '$http', '$cacheFactory', function
     }).then(function(response) {
       $scope.status = response.status;
       $scope.data = response.data;
-      arrAll = response.data.dip; //variable save array with all legislators in the api call
+      arrAll = response.data.dip; //variable save array with all legislators in the api call/
+
+      // arrAll = [{id_legislator_sil: '9219728',id_photo_sil: '9219728',
+      //           legislator_chamber_sil: 'diputado',
+      //           legislator_name_sil: 'Javier Octavio Herrera Borunda',
+      //           legislator_last_name_sil: 'Herrera Borunda',
+      //           legislator_first_name_sil: 'Javier Octavio',
+      //           legislator_status_sil: 'ACTIVO',
+      //           legislator_party_sil: 'PVEM',
+      //           legislator_score_sil: '100',
+      //           legislator_age_sil: '25/06/1980',
+      //           legislator_state_sil: ' Veracruz',
+      //           legislator_city_sil: ' Cosamaloapan',
+      //           legislator_election_sil: 'RP',
+      //           legislator_zone_sil: 'Veracruz',
+      //           legislator_protest_sil: '29/08/2015',
+      //           legislator_mail_sil: 'javier.herrera@congreso.gob.mx',
+      //           legislator_twitter_senate: 'javier.herrera@congreso.gob.mx',
+      //           legislator_supl_sil: '9219729'}];
+
       var legislstors_all = [];
-      $scope.filters = { };
+      $scope.filtersDeputys = { };
       console.log("Connected and Work Deputys :)", legislstors_all);
 
       if(arrAll){
         for (var i = 0; i < arrAll.length; i++) {
 
             val=arrAll[i];
+
 
             if(val.legislator_gender_senate=="Masculino"){
                 val.legislator_chamber_sil="Diputado";
@@ -39,8 +73,8 @@ app.controller('deputyController', ['$scope', '$http', '$cacheFactory', function
 
             var state = val.legislator_state_sil;
             var zone = val.legislator_zone_sil;
-            console.log("Estado"+ state);
-            console.log("Zona"+zone);
+            // console.log("Estado"+ state);
+            // console.log("Zona"+zone);
 
             if(state){
               val.legislator_state_sil = val.legislator_state_sil.trim().toLowerCase();
@@ -122,52 +156,41 @@ app.controller('deputyController', ['$scope', '$http', '$cacheFactory', function
               }
             }
 
+            if(val.legislator_election_sil == "RP" ){
+              val.legislator_election_sil = "Representación Proporcional"
+            }
 
-              if(val.legislator_election_sil == "RP" ){
-                val.legislator_election_sil = "Representación Proporcional"
-              }
+            if(val.legislator_election_sil == "MR"){
+              val.legislator_election_sil = "Mayoria Relativa"
+            }
 
-              if(val.legislator_election_sil == "MR"){
-                val.legislator_election_sil = "Mayoria Relativa"
-              }
+            if(val.legislator_election_sil == "PM"){
+              val.legislator_election_sil = "Representación Proporcional"
+            }
 
-              if(val.legislator_election_sil == "PM"){
-                val.legislator_election_sil = "Representación Proporcional"
-              }
+            if(val.legislator_facebook_sil){
+              $scope.classFacebook = "block";
+            }else{
+              $scope.classFacebook = "none";
+            }
 
-              var facebook = val.legislator_facebook_sil;
+            if(val.legislator_twitter_senate){
+              $scope.classTwitter = "block";
+            }else{
+              $scope.classFacebook = "none";
+            }
 
-                if(facebook == "FACEBOOK" || facebook == null){
-                  var class_none_facebook = "none";
-                }else{
-                      class_none_facebook = "block";
-                }
+            if(val.legislator_mail_sil){
+              $scope.classMail = "block";
+            }else{
+              $scope.classMail = "none";
+            }
+            //
 
-                $scope.classFacebook = class_none_facebook;
-
-              var twitter = val.legislator_twitter_senate;
-
-                if(twitter == null){
-                  var class_none_twitter = "none";
-                }else{
-                      class_none_twitter = "block";
-                }
-
-                $scope.classTwitter = class_none_twitter;
-
-
-              var mail = val.legislator_mail_sil;
-
-                if(mail == null){
-                  var class_none_mail = "none";
-                }else{
-                      class_none_mail = "block";
-                }
-
-                $scope.classMail = class_none_mail;
+            //
 
 
-            legislstors_all.push(val);
+          legislstors_all.push(val);
         }
         $scope.deputysApi=legislstors_all;
       }
@@ -182,7 +205,7 @@ app.controller('deputyController', ['$scope', '$http', '$cacheFactory', function
  app.controller('senatorController',  ['$scope', '$http', '$cacheFactory', function ($scope, $http, $cacheFactory) {
    var Cache = $cacheFactory('Cache', { capacity:0 });
      $http({
-       url: supurl+'/diputaos/sen/get',
+       url: supurl+'/diputados/sen/get',
        method: "POST",
        cache:Cache,
        headers : {'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'},
@@ -190,18 +213,162 @@ app.controller('deputyController', ['$scope', '$http', '$cacheFactory', function
      }).then(function(response) {
        $scope.status = response.status;
        $scope.data = response.data;
-       console.log("Connected and Work Senators :)");
        arrAll = response.data.dip;
+
+       // arrAll = [{id_legislator_sil: '9219728',id_photo_sil: '9219728',
+       //           legislator_chamber_sil: 'diputado',
+       //           legislator_name_sil: 'Javier Octavio Herrera Borunda',
+       //           legislator_last_name_sil: 'Herrera Borunda',
+       //           legislator_first_name_sil: 'Javier Octavio',
+       //           legislator_status_sil: 'ACTIVO',
+       //           legislator_party_sil: 'PVEM',
+       //           legislator_score_sil: '100',
+       //           legislator_age_sil: '25/06/1980',
+       //           legislator_state_sil: ' Veracruz',
+       //           legislator_city_sil: ' Cosamaloapan',
+       //           legislator_election_sil: 'RP',
+       //           legislator_zone_sil: 'Veracruz',
+       //           legislator_protest_sil: '29/08/2015',
+       //           legislator_mail_sil: 'javier.herrera@congreso.gob.mx',
+       //           legislator_facebook_sil: 'javier.herrera@congreso.gob.mx',
+       //           legislator_twitter_senate: 'javier.herrera@congreso.gob.mx',
+       //           legislator_supl_sil: '9219729'}];
+
+       var legislstors_all = [];
+       $scope.filtersSenators = { };
+       console.log("Connected and Work Senators :)", legislstors_all);
 
        if(arrAll){
          for (var i = 0; i < arrAll.length; i++) {
-           val=arrAll[i];
 
-           if(val.legislator_chamber_sil=="senador"){
-             $scope.senatorsApi=response.data.dip;
-             console.log(val.legislator_chamber_sil);
-           }
+             val=arrAll[i];
+
+             if(val.legislator_gender_senate=="Masculino"){
+                 val.legislator_chamber_sil="Diputado";
+             }
+
+             if(val.legislator_gender_senate=="Femenino"){
+                 val.legislator_chamber_sil="Diputada";
+             }
+
+             var state = val.legislator_state_sil;
+             var zone = val.legislator_zone_sil;
+             // console.log("Estado"+ state);
+             // console.log("Zona"+zone);
+
+             if(state){
+               val.legislator_state_sil = val.legislator_state_sil.trim().toLowerCase();
+               if(val.legislator_state_sil == "baja california sur"){
+                 val.legislator_state_sil = "baja california s."
+               }
+
+               if(val.legislator_state_sil == "s/e"){
+                 val.legislator_state_sil = "cdmx"
+               }
+               if(val.legislator_state_sil == "df"){
+                 val.legislator_state_sil = "cdmx"
+               }
+               if(val.legislator_state_sil == "méxico"){
+                 val.legislator_state_sil = "cdmx"
+               }
+               if(val.legislator_state_sil == "distrito federal"){
+                 val.legislator_state_sil = "cdmx"
+               }
+             }
+
+             if(zone){
+               val.legislator_zone_sil = val.legislator_zone_sil.trim().toLowerCase();
+
+               if(val.legislator_zone_sil == "baja california sur"){
+                 val.legislator_zone_sil = "bcs"
+               }
+
+               if(val.legislator_zone_sil == "baja california"){
+                 val.legislator_zone_sil = "bc"
+               }
+
+               if(val.legislator_zone_sil == "s/e"){
+                 val.legislator_zone_sil = "cdmx"
+               }
+
+               if(val.legislator_zone_sil == "df"){
+                 val.legislator_zone_sil = "cdmx"
+               }
+
+               if(val.legislator_zone_sil == "m&#xe9;xico"){
+                 val.legislator_zone_sil = "cdmx"
+               }
+
+               if(val.legislator_zone_sil == "distrito federal"){
+                 val.legislator_zone_sil = "cdmx"
+               }
+
+               if(val.legislator_zone_sil == "michoac&#xe1;n"){
+                 val.legislator_zone_sil = "michoacan"
+               }
+
+               if(val.legislator_zone_sil == "nuevo le&#xf3;n"){
+                 val.legislator_zone_sil = "nl"
+               }
+
+               if(val.legislator_zone_sil == "quintana roo"){
+                 val.legislator_zone_sil = "qr"
+               }
+
+               if(val.legislator_zone_sil == "yucat&#xe1;n"){
+                 val.legislator_zone_sil = "yucatan"
+               }
+
+               if(val.legislator_zone_sil == "n/a"){
+                 val.legislator_zone_sil = "cdmx"
+               }
+
+               if(val.legislator_zone_sil == "quer&#xe9;taro"){
+                 val.legislator_zone_sil = "queretaro"
+               }
+
+               if(val.legislator_zone_sil == "aguascalientes"){
+                 val.legislator_zone_sil = "agc"
+               }
+
+               if(val.legislator_zone_sil == "san luis potos&#xed;"){
+                 val.legislator_zone_sil = "slp"
+               }
+             }
+
+             if(val.legislator_election_sil == "RP" ){
+               val.legislator_election_sil = "Representación Proporcional"
+             }
+
+             if(val.legislator_election_sil == "MR"){
+               val.legislator_election_sil = "Mayoria Relativa"
+             }
+
+             if(val.legislator_election_sil == "PM"){
+               val.legislator_election_sil = "Representación Proporcional"
+             }
+
+             if(val.legislator_facebook_sil){
+               $scope.classFacebook = "block";
+             }else{
+               $scope.classFacebook = "none";
+             }
+
+             if(val.legislator_twitter_senate){
+               $scope.classTwitter = "block";
+             }else{
+               $scope.classFacebook = "none";
+             }
+
+             if(val.legislator_mail_sil){
+               $scope.classMail = "block";
+             }else{
+               $scope.classMail = "none";
+             }
+
+             legislstors_all.push(val);
          }
+         $scope.senatorsApi=legislstors_all;
        }
 
      }, function(response) {
